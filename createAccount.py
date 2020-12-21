a1b9fd91dfff91b083988b848e81521da33dbb37
Juan José <jjrd91@hotmail.com>
2020-12-21 09:48:11 -0500

    createAccount.py and login.py completed

--- createAccount.py
+++ createAccount.py
@@ -1,19 +1,22 @@
 import unittest
 from selenium import webdriver
 from pyunitreport import HTMLTestRunner
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
 
 class CreateAccountTest(unittest.TestCase):
     
     def setUp(self):
-        self.driver = webdriver.Chrome(executable_path = r'C://Users//USER//proyectos//selenium//assertions//chromedriver.exe')
+        self.driver = webdriver.Chrome(executable_path = r'C://chromedriver.exe')
         driver = self.driver
         driver.implicitly_wait(15)
         driver.maximize_window()
         driver.get("http://automationpractice.com/")
 
     def test_user_clicks_on_sign_in(self):
-        login_button = self.driver.find_element_by_xpath('//*[@id="header"]/div[2]/div/div/nav/div[1]/a')
-        login_button.click()
+        sign_in_button = self.driver.find_element_by_xpath('//*[@id="header"]/div[2]/div/div/nav/div[1]/a')
+        sign_in_button.click()
     
     def test_user_enters_email(self):
         email_field = self.driver.find_element_by_name('email_create')
@@ -29,18 +32,98 @@ class CreateAccountTest(unittest.TestCase):
 
         title = driver.find_element_by_id('id_gender1')
         title.click()
+
+        first_name_1 = driver.find_element_by_name('customer_firstname')
+        first_name_1.clear()
+        first_name_1.send_keys('Juan José')
+
+        last_name_1 = driver.find_element_by_name('customer_lastname')
+        last_name_1.clear()
+        last_name_1.send_keys('Rodríguez')
+
+        email = driver.find_element_by_id('email')
+        email.clear()
+        email.send_keys('juanjoserodriguezduque@gmail.com')
+
+        password = driver.find_element_by_id('passwd')
+        password.clear()
+        password.send_keys('1234567890')
+
+        email = driver.find_element_by_id('email')
+        email.clear()
+        email.send_keys('juanjoserodriguezduque@gmail.com')
+
+        days = Select(self.driver.find_element_by_id('days'))
+        days.select_by_index(27)
+
+        months = Select(self.driver.find_element_by_id('months'))
+        months.select_by_index(2)
+
+        first_name_2 = driver.find_element_by_id('firstname')
+        first_name_2.clear()
+        first_name_2.send_keys('Juan José')
+
+        last_name_2 = driver.find_element_by_id('lastname')
+        last_name_2.clear()
+        last_name_2.send_keys('Rodríguez')
+
+        company = driver.find_element_by_id('company')
+        company.clear()
+        company.send_keys('Freelance')
+
+        address_1 = driver.find_element_by_id('address1')
+        address_1.clear()
+        address_1.send_keys('797-799 5th Ave')
+
+        address_2 = driver.find_element_by_id('address2')
+        address_2.clear()
+        address_2.send_keys('Apartment 1001')
+
+        city = driver.find_element_by_id('city')
+        city.clear()
+        city.send_keys('New York')
+
+        state = Select(self.driver.find_element_by_id('id_state'))
+        state.select_by_index(33)
+
+        postal_code = driver.find_element_by_id('postcode')
+        postal_code.clear()
+        postal_code.send_keys('10016')
+
+        country = Select(self.driver.find_element_by_id('id_country'))
+        country.select_by_index(1)
+
+        additional_information = driver.find_element_by_name('other')
+        additional_information.clear()
+        additional_information.send_keys('Thank you!')
+
+        home_phone = driver.find_element_by_name('phone')
+        home_phone.clear()
+        home_phone.send_keys('5864139')
         
-        first_name = driver.find_element_by_name('customer_firstname')
-        first_name.clear()
-        first_name.send_keys('Juan José')
-        last_name = driver.find_element_by_name('customer_lastname')
-        last_name.clear()
-        last_name.send_keys('Rodríguez')
+        mobile_phone = driver.find_element_by_name('phone_mobile')
+        mobile_phone.clear()
+        mobile_phone.send_keys('+57 3045385895')
 
+        alias = driver.find_element_by_name('alias')
+        alias.clear()
+        alias.send_keys('juanjo')
 
+        register_button = driver.find_element_by_name('submitAccount')
+        register_button.click()
+
+    def test_user_is_taken_my_account_page(self):
+        self.assertTrue(self.is_element_present(By.XPATH, '//*[@id="header"]/div[2]/div/div/nav/div[1]/a/span'))
 
     def tearDown(self):
         self.driver.quit()
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by = how, value = what)
+        except NoSuchElementException as variable:
+            return False
+        return True
 
 if __name__ == "__main__":
         unittest.main(verbosity = 2, testRunner = HTMLTestRunner(output = 'reports', report_name = 'createAccount-report'))
--- login.py
+++ login.py
@@ -1,35 +1,47 @@
 import unittest
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
 
 class LoginTest(unittest.TestCase):
     
     def setUp(self):
-        self.driver = webdriver.Chrome(executable_path = r'C://Users//USER//proyectos//selenium//assertions//chromedriver.exe')
+        self.driver = webdriver.Chrome(executable_path = r'C://chromedriver.exe')
         driver = self.driver
-        driver.implicitly_wait(30)
+        driver.implicitly_wait(15)
         driver.maximize_window()
-        driver.get("http://demo-store.seleniumacademy.com/")
+        driver.get("http://automationpractice.com/")
 
-    def test_search_tee(self):
-        driver = self.driver
-        search_field = driver.find_element_by_name('q')
-        search_field.clear()
+    def test_user_clicks_on_sign_in(self):
+        sign_in_button = self.driver.find_element_by_xpath('//*[@id="header"]/div[2]/div/div/nav/div[1]/a')
+        sign_in_button.click()
 
-        search_field.send_keys('tee')
-        search_field.submit()
+    def test_user_enters_email(self):
+        email_login = self.driver.find_element_by_id('email')
+        email_login.clear()
+        email_login.send_keys('juanjoserodriguezduque@gmail.com')
+    
+    def test_user_enters_password(self):
+        password_login = self.driver.find_element_by_xpath('//*[@id="passwd"]')
+        password_login.clear()
+        password_login.send_keys('1234567890')
 
-    def test_search_salt_shaker(self):
-        driver = self.driver
-        search_field = driver.find_element_by_name('q')
+    def test_user_clicks_sign_in(self):
+        submit_login = self.driver.find_element_by_id('SubmitLogin')
+        submit_login.click()
 
-        search_field.send_keys('salt shaker')
-        search_field.submit()
-
-        products = driver.find_elements_by_xpath('//*[@id="top"]/body/div/div[2]/div[2]/div/div[2]/div[2]/div[3]/ul/li/div/h2/a')
-        self.assertEqual(1, len(products))
+    def test_the_user_is_successfully_logged_in(self):
+        self.assertTrue(self.is_element_present(By.XPATH, '//*[@id="header"]/div[2]/div/div/nav/div[1]/a/span'))
 
     def tearDown(self):
         self.driver.quit()
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by = how, value = what)
+        except NoSuchElementException as variable:
+            return False
+        return True
     
 
 if __name__ == "__main__":
